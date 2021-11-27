dde6e7a0ff6baad5d362f599bcbc4adae830103b
Yazhou-Z <76484768+Yazhou-Z@users.noreply.github.com>
2021-11-27 23:51:42 +0800

    add function: A_load, B_load, print_Rules_intro, debug print and random_init.
    Add a file named "test.cpp" for debugging.

--- body.cpp
+++ body.cpp
@@ -3,32 +3,53 @@
 #include <iostream>
 #include <random>
 #include <time.h>
+#include <fstream>
 #include "body.h"
 using namespace std;
 
 
+// 已经 de 完 bug
 // Function: print the whole board.
 // No input.
 // Output: the whole board.
 void print(int map[8][8]){
     for (int y = 0; y < 8; y ++) {
         cout << " ";
+        
         for (int x = 0; x < 8; x ++) {
             if (map[y][x] == 0) cout << "_ ";
-            if (map[y][x] == 1) cout << "x ";
-            if (map[y][x] == 2) cout << "o ";
+            if (map[y][x] == 1) cout << "x ";   // player A
+            if (map[y][x] == 2) cout << "o ";   // player B
         }
+
         cout << endl;
     }
-};
-
+}
 
 // A 下棋
-void A_load(int map[8][8], Point A);
+// Function: get the position player A chooses, load it to the board
+// Input: A postison, the board
+// No output while change the board
+void A_load(int map[8][8], Point A){
+    int x, y;
+    x = A.x;
+    y = A.y;
+    map[y][x] = 1;
+}
 
 
 // B 下棋
-void B_load(int map[8][8], Point B);
+// Function: get the position player B chooses, load it to the board
+// Input: B postison, the board
+// No output while change the board
+void B_load(int map[8][8], Point B){
+    int x, y;
+    x = B.x;
+    y = B.y;
+    map[y][x] = 2;
+}
+
+
 
 // Function: check if 棋子下到非空白位置
 // 防止有人将棋子下到非空白位置
@@ -37,7 +58,7 @@ bool reload(Point i, int map[8][8]);
 
 // Function: check if the game is over.
 // Input: a board
-// Output: 
+// Output: whether game over
 bool gameOver(int map[8][8]);
 
 
@@ -56,10 +77,30 @@ bool full(int map[8][8]){
 }
 
 
+// Function: read and print the rule from the file: "rule.txt".
+// No input, while read a file
+// No output, while print the text in the file "rule.txt"
 void print_Rules_intro(){
+    ifstream fin;
+	fin.open("rule.txt");
+
+    if ( fin.fail() ){
+		cout << "Error in file opening!" 
+			<< endl;
+ 		exit(1);
+ 	}
+    
+    string line;
+
+	while ( getline(fin, line) ) {
+		cout << line << endl;
+ 	}
+
+	fin.close();
 }
 
 
+// 已经 de 完 bug
 // Function: init the board, randomly decide which palyer should go first and the position of the first piece.
 // Input: an empty board
 // Output: which palyer play first. if AB = 0, A first, else, B first.
@@ -76,8 +117,8 @@ int random_init(int map[8][8]){
     int y = (rand() % 8);
     int AB = (rand() % 2);
 
-    if (AB == 0) map[y][x] = 1;
-    else map[y][x] = 2;
+    if (AB == 0) map[y][x] = 1;     // player A first
+    else map[y][x] = 2;             // player B first
 
     return AB;
 }
