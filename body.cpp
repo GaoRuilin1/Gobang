086b11c629a60089a3cb339a7b53821fe194d548
GaoRuilin1 <80329948+GaoRuilin1@users.noreply.github.com>
2021-12-02 20:53:54 +0800

    Update body.cpp
    use a new function formConsecutive5 to judge whether the game ends
    change dimension to 15
    make B_load function pass by reference

--- body.cpp
+++ body.cpp
@@ -7,9 +7,9 @@
 #include "body.h"
 using namespace std;
 
-const int num = 8;
+const int num = 15;
 
-// 已经 de 完 bug
+
 // Function: print the whole board.
 // No input.
 // Output: the whole board.
@@ -17,8 +17,8 @@ void print(int map[15][15]){
     for (int y = 0; y < 15; y ++) {
         cout << " ";
 
-        for (int x = 0; x < 8; x ++) {
-            if (map[y][x] == 0) cout << "_ ";
+        for (int x = 0; x < 15; x ++) {
+            if (map[y][x]>=0 ) cout << "_ ";
             if (map[y][x] == -1) cout << "x ";   // player A
             if (map[y][x] == -2) cout << "o ";   // player B
         }
@@ -31,11 +31,11 @@ void print(int map[15][15]){
 // Function: get the position player A chooses, load it to the board
 // Input: A postison, the board
 // No output while change the board
-void A_load(int map[15][15], Point A){
+void A_load(int map[15][15], Point *A){
     int x, y;
-    x = A.x;
-    y = A.y;
-    map[y][x] = 1;
+    x = A->x;
+    y = A->y;
+    map[y][x] = -1;
 }
 
 
@@ -43,97 +43,92 @@ void A_load(int map[15][15], Point A){
 // Function: get the position player B chooses, load it to the board
 // Input: B postison, the board
 // No output while change the board
-void B_load(int map[15][15], Point B){
+void B_load(int map[15][15], Point *B){
     int x, y;
-    x = B.x;
-    y = B.y;
-    map[y][x] = -1;
+    x = B->x;
+    y = B->y;
+    map[y][x] = -2;
 }
 
 
 
-// Function: check if 棋子下到非空白位置
-// 防止有人将棋子下到非空白位置
-bool reload(Point i, int map[8][8]);
 
+bool formConsecutive5(int board[15][15], int row, int col){
+  //roll
+    for(int i = 0; i < 5; i++){
+    if(col - i >= 0 &&
+        col - i + 4 < 15 &&
+        board[row][col-i] == board[row][col-i+1] &&
+        board[row][col-i] == board[row][col-i+2] &&
+        board[row][col-i] == board[row][col-i+3] &&
+        board[row][col-i] == board[row][col-i+4]){return true;}
+    }
+
+  //column
+    for(int i = 0; i < 5; i++){
+    if(row - i >= 0 &&
+        row - i + 4 < 15 &&
+        board[row-i][col] == board[row-i+1][col] &&
+        board[row-i][col] == board[row-i+2][col] &&
+        board[row-i][col] == board[row-i+3][col] &&
+        board[row-i][col] == board[row-i+4][col]){return true;}
+}
+
+  // diaginal 1 lower left to upper right
+    for(int i = 0; i < 5; i++){
+        if(row + i < 15 &&
+            row + i - 4 >= 0 &&
+            col - i >= 0 &&
+            col - i + 4 < 15 &&
+            board[row+i][col-i] == board[row+i-1][col-i+1] &&
+            board[row+i][col-i] == board[row+i-2][col-i+2] &&
+            board[row+i][col-i] == board[row+i-3][col-i+3] &&
+            board[row+i][col-i] == board[row+i-4][col-i+4]){return true;}
+}
+
+  //  the othe diginal 
+    for(int i = 0; i < 5; i++){
+    if(row - i >= 0 &&
+        row - i - 4 < 15 &&
+        col - i >= 0 &&
+        col - i + 4 < 15 &&
+        board[row-i][col-i] == board[row-i+1][col-i+1] &&
+        board[row-i][col-i] == board[row-i+2][col-i+2] &&
+        board[row-i][col-i] == board[row-i+3][col-i+3] &&
+        board[row-i][col-i] == board[row-i+4][col-i+4]){return true;}
+    }
+    return false;
+
+}
 
 // Function: check if the game is over.
 // Input: a board
 // Output: whether game over
-int gameOver(int map[8][8]) {
-
-    // five pieces in the same line.
-    int line = 0;
-    for (int y = 0; y < num; y++) {
-        for (int x = 0; x < num - 5; x++) {
-            for (int i = 0; i < 5; i ++) {
-                line += map[y][x + i];
-            } 
+bool gameOver(int map[15][15]) {
+    for (int row = 0; row < 15; row++)
+    {
+        for (int col = 0; col < 15; col++)
+        {
+        
+            if (formConsecutive5(map, row, col) == 1) {return 1;}
+            }
         }
-    }
-
-    if (line == 4 ) {
-        return 1;
-    }
-    else if (line == -4) {
-        return 2;
-    }
-
-    // five piece in the same column
-    int column = 0;
-    for (int y = 0; y < num - 5; y++) {
-        for (int x = 0; x < num; x++) {
-            for (int i = 0; i < 5; i ++) {
-                column += map[y + i][x];
-            } 
-        }
-    }
-
-    if (column == 4 ) {
-        return 1;
-    }
-    else if (column == -4) {
-        return 2;
-    }
-
-    // 
-    int de = 0;
-    for (int y = 0; y < num - 5; y++) {
-        for (int x = 0; x < num - 5; x++) {
-            for (int i = 0; i < 5; i ++) {
-                column += map[y + i][x + 1];
-            } 
-        }
-    }
-    
-    if (de == 4 ) {
-        return 1;
-    }
-    else if (de == -4) {
-        return 2;
-    }
-
-    if (full(map)) return 3;
-
     return 0;
 }
-
 
 // Function: chaeck if the board is full
 // Input: a board
 // Output: whether the board is full
-bool full(int map[8][8]){
+bool full(int map[15][15]){
     bool if_full = 1;
-    for (int y = 0; y < 8; y ++) {
-        for (int x = 0; x < 8; x ++) {
-            if (map[y][x] == 0) if_full = 0;
+    for (int y = 0; y < 15; y ++) {
+        for (int x = 0; x < 15; x ++) {
+            if (map[y][x] > 0) if_full = 0;
         }
     }
 
     return if_full;
 }
-
-
 // Function: read and print the rule from the file: "rule.txt".
 // No input, while read a file
 // No output, while print the text in the file "rule.txt"
@@ -156,8 +151,6 @@ void print_Rules_intro(){
 	fin.close();
 }
 
-
-// 已经 de 完 bug
 // Function: init the board, randomly decide which palyer should go first and the position of the first piece.
 // Input: an empty board
 // Output: which palyer play first. if AB = 0, A first, else, B first.
